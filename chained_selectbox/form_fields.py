ff1ed474f1550643277e5bb2bddfaa4b933aa7fb
rizwanmeo <rizwan_meo@rocketmail.com>
2014-03-27 12:01:36 +0500

    chained multiselect support

--- chained_selectbox/form_fields.py
+++ chained_selectbox/form_fields.py
@@ -1,19 +1,40 @@
-from widgets import ChainedSelect
-from django.forms import ChoiceField
+from widgets import ChainedSelect, ChainedSelectMultiple
 from django_filters.filters import Filter
+from django.forms import ChoiceField, MultipleChoiceField
 
 
 class ChainedChoiceField(ChoiceField):
-    def __init__(self, parent_field, ajax_url, choices=None, *args, **kwargs):
+    def __init__(self, parent_field, ajax_url, grand_parent_field=None, choices=None, *args, **kwargs):
 
         self.parent_field = parent_field
         self.ajax_url = ajax_url
         self.choices = choices or (('', '--------'), )
-        self.widget = ChainedSelect(parent_field=parent_field, ajax_url=ajax_url)
+        self.grand_parent_field = grand_parent_field
+        self.widget = ChainedSelect(parent_field=parent_field,
+                                    ajax_url=ajax_url)
         defaults = {'widget': self.widget}
         defaults.update(kwargs)
 
         super(ChainedChoiceField, self).__init__(choices=self.choices, *args, **defaults)
+
+
+    def valid_value(self, value):
+        "Dynamic choices so just return True for now"
+        return True
+
+
+class ChainedMultipleChoiceField(MultipleChoiceField):
+    def __init__(self, parent_field, ajax_url, grand_parent_field=None, choices=None, *args, **kwargs):
+        self.parent_field = parent_field
+        self.ajax_url = ajax_url
+        self.choices = choices or ()
+        self.grand_parent_field = grand_parent_field
+        self.widget = ChainedSelectMultiple(parent_field=parent_field,
+                                            ajax_url=ajax_url)
+        defaults = {'widget': self.widget}
+        defaults.update(kwargs)
+
+        super(ChainedMultipleChoiceField, self).__init__(choices=self.choices, *args, **defaults)
 
 
     def valid_value(self, value):
