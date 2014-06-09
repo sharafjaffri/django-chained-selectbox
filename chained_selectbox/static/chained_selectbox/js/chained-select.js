15a035ff32d0028b8735bde8ca5de189ef184603
Sharaf Ali <sharafjaffri@live.com>
2014-06-09 20:49:21 +0500

    Update chained-select.js

--- chained_selectbox/static/chained_selectbox/js/chained-select.js
+++ chained_selectbox/static/chained_selectbox/js/chained-select.js
@@ -6,26 +6,29 @@
                 chained_id = chained_id.replace('__prefix__', $(this).attr('name').split('-')[1]);
                 $(this).attr('chained_id', chained_id);
             }
-
             var valuefield = $('#' + chained_id);
             var ajax_url = valuefield.attr('ajax_url');
-
             $.get(ajax_url, {field: valuefield.attr('name').split('-')[2], parent_value: $(this).val()}, function(j) {
                 var options = '';
+                var options_li = '';
                 for (var i = 0; i < j.length; i++) {
-                    options += '<option value="' + j[i][0] + '">' + j[i][1] + '</option>';
+                    options += '<option value="' + j[i][0] + '" >' + j[i][1] + '</option>';
                 }
-                valuefield.html(options);
+				valuefield.html(options);
+                valuefield.find('.available').html(options);
                 valuefield.trigger('change');
-            }, "json");
+				if(valuefield.attr('class')=='multiselect'){
+					valuefield.nextAll().find('a.remove-all')[0].click();
+					
+				}
+				}, "json");
         }
 
         $('.chained-parent-field').live('change', function(e) {
+            var that = this;
             $(this).loadChainedChoices();
         });
+
     });
+		
 })(django.jQuery);
-
-
-
-
